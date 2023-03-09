e3e609ba57eaaa812f0fa2665b13f99c9e0e7635
Eugene Ilin <eugenepianist@gmail.com>
2023-03-09 10:16:07 -0500

    made general overview, needs work and refactoring of course...

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import Home from './components/pages/Home';
 import Enter from './components/pages/Enter';
+import Scores from './components/pages/Scores';
 import UpcomingPerformances from './components/pages/UpcomingPerformances';
 
 function App() {
@@ -13,11 +14,8 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/enter' component={Enter} />
-          <Route
-            exact
-            path='/upcoming-performances'
-            component={UpcomingPerformances}
-          />
+          <Route exact path='/scores' component={Scores} />
+          <Route exact path='/upcoming-performances' component={UpcomingPerformances} />
         </Switch>
       </Fragment>
     </Router>
